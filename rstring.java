09daed0d8ac0b6b520cf4b05aee171e8ef84cc59
sai-890 <101650449+sai-890@users.noreply.github.com>
2022-03-16 14:47:15 +0530

    Add files via upload

---
+++ rstring.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class rstring {
+
+	public static void main(String[] args) {
+		int i;
+		String s="hai",k="";
+		for(i=0;i<s.length();i++) {
+			char j=s.charAt(i);
+			k=j+k;
+		}
+        System.out.println(k);        
+        
+        String str="hello";
+        StringBuilder t=new StringBuilder();
+        t.append(str);
+        t.reverse();
+        System.out.println(t);
+	}
+}
